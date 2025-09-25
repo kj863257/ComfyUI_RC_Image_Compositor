9a6e6cb945526d6f34c361c3c6100404d24b6769
kj863257rc <kj863257@163.com>
2025-09-25 14:00:07 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .rc_image_compositor import RC_Image_Compositor
+
+NODE_CLASS_MAPPINGS = {"RC_Image_Compositor": RC_Image_Compositor}
+NODE_DISPLAY_NAME_MAPPINGS = {"RC_Image_Compositor": "RC 图像合成器 (Photoshop 混合模式)"}
