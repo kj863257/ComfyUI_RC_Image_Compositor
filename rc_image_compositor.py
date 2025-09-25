9aa608e847c317311258d293e606118613d7a2cc
kj863257rc <kj863257@163.com>
2025-09-25 15:09:10 +0800

    Update rc_image_compositor.py

--- rc_image_compositor.py
+++ rc_image_compositor.py
@@ -314,12 +314,3 @@ class LoadImageWithAlpha:
             return "Invalid image file: {}".format(image)
         return True
 
-NODE_CLASS_MAPPINGS = {
-    "RC_Image_Compositor": RC_Image_Compositor,
-    "RC_LoadImageWithAlpha": LoadImageWithAlpha
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "RC_Image_Compositor": "RC 图像合成器 (Photoshop 混合模式)",
-    "RC_LoadImageWithAlpha": "RC 加载图像（带透明通道）"
-}
