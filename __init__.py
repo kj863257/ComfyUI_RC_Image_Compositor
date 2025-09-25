b08893c6e09df7ce2103c2d4361e045bdf77098f
kj863257rc <kj863257@163.com>
2025-09-25 15:12:12 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+NODE_CLASS_MAPPINGS = {
+    "RC_Image_Compositor": RC_Image_Compositor,
+    "RC_LoadImageWithAlpha": LoadImageWithAlpha
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "RC_Image_Compositor": "RC 图像合成器 (Photoshop 混合模式)",
+    "RC_LoadImageWithAlpha": "RC 加载图像（带透明通道）"
+}
